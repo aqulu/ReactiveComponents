ec7b8dd407137d1dd1d291470f26fc763c3c5858
Luca Aquino <luca-aquino@owndays.co.jp>
2017-09-20 18:36:46 +0900

    added customization options

--- library/src/main/java/lu/aqu/reactivecomponents/ReactiveRecyclerView.java
+++ library/src/main/java/lu/aqu/reactivecomponents/ReactiveRecyclerView.java
@@ -2,6 +2,7 @@ package lu.aqu.reactivecomponents;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.view.View;
@@ -17,8 +18,10 @@ import android.widget.TextView;
  */
 public class ReactiveRecyclerView extends RecyclerView {
 
+    private View mEmptyView;
+    private View mProgressView;
+
     private Adapter mItemAdapter;
-    private SingleViewAdapter mEmptyAdapter;
 
     public ReactiveRecyclerView(Context context) {
         this(context, null);
@@ -32,36 +35,33 @@ public class ReactiveRecyclerView extends RecyclerView {
         super(context, attrs, defStyle);
         TypedArray a = getContext().getTheme().obtainStyledAttributes(attrs, R.styleable.ReactiveRecyclerView, 0, 0);
 
-        String emptyString;
+        mProgressView = new ProgressBar(getContext(), null, android.R.attr.progressBarStyleLarge);
+
+        TextView emptyView = new TextView(getContext());
         try {
-            emptyString = a.getString(R.styleable.ReactiveRecyclerView_emptyText);
+            if (a.getBoolean(R.styleable.ReactiveRecyclerView_autoShowProgress, true)) {
+                showProgressView();
+            }
+
+            emptyView.setText(a.getString(R.styleable.ReactiveRecyclerView_emptyText));
+            if (a.hasValue(R.styleable.ReactiveRecyclerView_emptyTextSize)) {
+                int textSize = a.getDimensionPixelSize(R.styleable.ReactiveRecyclerView_emptyTextSize,
+                        (int) emptyView.getTextSize());
+                emptyView.setTextSize(textSize);
+            }
         } finally {
             a.recycle();
         }
 
-        TextView emptyView = new TextView(getContext());
-        emptyView.setText(emptyString);
-        mEmptyAdapter = new SingleViewAdapter(emptyView);
-
-        ProgressBar progressBar = new ProgressBar(getContext(), null,
-                android.R.attr.progressBarStyleLarge);
-        super.setAdapter(new SingleViewAdapter(progressBar));
-    }
-
-    private void toggleAdapters() {
-        if (mItemAdapter != null && mItemAdapter.getItemCount() > 0) {
-            super.setAdapter(mItemAdapter);
-        } else {
-            super.setAdapter(mEmptyAdapter);
-        }
+        mEmptyView = emptyView;
     }
 
     @Override
-    public void setAdapter(final Adapter adapter) {
-        this.mItemAdapter = adapter;
+    public void setAdapter(Adapter adapter) {
+        mItemAdapter = adapter;
 
-        if (adapter != null) {
-            adapter.registerAdapterDataObserver(new AdapterDataObserver() {
+        if (mItemAdapter != null) {
+            mItemAdapter.registerAdapterDataObserver(new AdapterDataObserver() {
                 @Override
                 public void onChanged() {
                     toggleAdapters();
@@ -82,8 +82,50 @@ public class ReactiveRecyclerView extends RecyclerView {
         }
     }
 
-    private static class SingleViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    /**
+     * shows the progressview (if LayoutManager has been set for this RecyclerView instance)
+     */
+    public void showProgressView() {
+        super.setAdapter(new SingleViewAdapter(mProgressView));
+    }
 
+    /**
+     * set the view to display as progress (when no adapter has been set yet)
+     *
+     * @param progressView view to be displayed when no adapter has been asigned yet
+     */
+    public void setProgressView(@NonNull View progressView) {
+        mProgressView = progressView;
+    }
+
+    /**
+     * @return true, if user has set an adapter, false otherwise
+     */
+    public boolean hasAdapter() {
+        return mItemAdapter != null;
+    }
+
+    /**
+     * set the view to display when set item adapter does not contain any items
+     *
+     * @param emptyView view to be displayed when adapter is empty
+     */
+    public void setEmptyView(@NonNull View emptyView) {
+        mEmptyView = emptyView;
+        if (hasAdapter()) {
+            toggleAdapters();
+        }
+    }
+
+    private void toggleAdapters() {
+        if (mItemAdapter != null && mItemAdapter.getItemCount() > 0) {
+            super.setAdapter(mItemAdapter);
+        } else {
+            super.setAdapter(new SingleViewAdapter(mEmptyView));
+        }
+    }
+
+    private static class SingleViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         private View view;
 
         private SingleViewAdapter(View view) {
@@ -95,6 +137,7 @@ public class ReactiveRecyclerView extends RecyclerView {
             view.post(new Runnable() {
                 @Override
                 public void run() {
+                    // set padding as soon as view can be measured to center inside recyclerview
                     int paddingVertical = (parent.getHeight() - view.getHeight()) / 2;
                     int paddingHorizontal = (parent.getWidth() - view.getWidth()) / 2;
                     view.setPadding(paddingHorizontal, paddingVertical,
