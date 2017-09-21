b27d4580021902663f608b36f6f802b639493b7a
Luca Aquino <luca-aquino@owndays.co.jp>
2017-09-21 18:59:40 +0900

    added sizing option to loading fab

--- library/src/main/java/lu/aqu/reactivecomponents/LoadingFloatingActionButton.java
+++ library/src/main/java/lu/aqu/reactivecomponents/LoadingFloatingActionButton.java
@@ -4,28 +4,39 @@ import android.content.Context;
 import android.content.res.ColorStateList;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
+import android.support.annotation.IntDef;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
 public class LoadingFloatingActionButton extends RelativeLayout {
 
     private FloatingActionButton mFab;
     private ProgressBar mProgressBar;
 
+    public static final int SIZE_MINI = FloatingActionButton.SIZE_MINI;
+    public static final int SIZE_NORMAL = FloatingActionButton.SIZE_NORMAL;
+
+    @Retention(RetentionPolicy.SOURCE)
+    @IntDef({SIZE_MINI, SIZE_NORMAL})
+    public @interface Size {
+    }
+
     public LoadingFloatingActionButton(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         setClipChildren(false);
+        LayoutInflater inflater = LayoutInflater.from(context);
 
-        View view = LayoutInflater.from(context)
-                .inflate(R.layout.loading_floating_action_button, this, true);
-        mFab = (FloatingActionButton) view.findViewById(R.id.fab);
-        mProgressBar = (ProgressBar) view.findViewById(R.id.fab_progress);
+        mFab = new FloatingActionButton(context);
 
         TypedArray a = context.getTheme()
                 .obtainStyledAttributes(attrs, R.styleable.LoadingFloatingActionButton, 0, 0);
@@ -34,18 +45,28 @@ public class LoadingFloatingActionButton extends RelativeLayout {
             if (a.hasValue(R.styleable.LoadingFloatingActionButton_backgroundColor)) {
                 int background = a.getColor(R.styleable.LoadingFloatingActionButton_backgroundColor,
                         getPrimaryDarkColor(context));
-
                 mFab.setBackgroundTintList(ColorStateList.valueOf(background));
             }
 
-            Drawable drawable;
             if (a.hasValue(R.styleable.LoadingFloatingActionButton_src)) {
-                drawable = a.getDrawable(R.styleable.LoadingFloatingActionButton_src);
+                Drawable drawable = a.getDrawable(R.styleable.LoadingFloatingActionButton_src);
                 mFab.setImageDrawable(drawable);
             }
+
+            int size = a.getInt(R.styleable.LoadingFloatingActionButton_loadingFabSize, FloatingActionButton.SIZE_NORMAL);
+            if (size == SIZE_MINI) {
+                mProgressBar = (ProgressBar) inflater.inflate(R.layout.progress_bar_mini, null);
+            } else {
+                mProgressBar = (ProgressBar) inflater.inflate(R.layout.progress_bar_normal, null);
+            }
+            mFab.setSize(size);
         } finally {
             a.recycle();
         }
+
+        mProgressBar.setVisibility(INVISIBLE);
+        addViewCentered(mFab);
+        addViewCentered(mProgressBar);
 
         mFab.setOnClickListener(new OnClickListener() {
             @Override
@@ -53,6 +74,13 @@ public class LoadingFloatingActionButton extends RelativeLayout {
                 callOnClick();
             }
         });
+    }
+
+    private void addViewCentered(View view) {
+        LayoutParams layoutParams = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+        addView(view, layoutParams);
     }
 
     private int getPrimaryDarkColor(Context context) {
